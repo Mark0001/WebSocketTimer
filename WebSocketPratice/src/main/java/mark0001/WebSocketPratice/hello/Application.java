58a380d0805953b2b17b022d518352887694d004
chihshengshih <chihsheng.shih@iisigroup.com>
2018-02-07 16:39:16 +0800

    remove java TimeTask,using spring @Schedile

--- WebSocketPratice/src/main/java/mark0001/WebSocketPratice/hello/Application.java
+++ WebSocketPratice/src/main/java/mark0001/WebSocketPratice/hello/Application.java
@@ -2,8 +2,10 @@ package mark0001.WebSocketPratice.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
--- WebSocketPratice/src/main/java/mark0001/WebSocketPratice/hello/WebSocketUtil.java
+++ WebSocketPratice/src/main/java/mark0001/WebSocketPratice/hello/WebSocketUtil.java
@@ -1,15 +1,12 @@
 package mark0001.WebSocketPratice.hello;
 
 import java.util.Date;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -20,22 +17,10 @@ public class WebSocketUtil {
     @Autowired
     private final SimpMessagingTemplate messageTemplate = null;
 
-    @PostConstruct
-    public void init() {
-        logger.info("WebSocketUtil Initialized");
-        final Timer timer = new Timer();
-        timer.schedule(new DateTask(), 1000, 1000);
-    }
-
-    //    public void sendTime() {
-    //        WebSocketUtil.this.messageTemplate.convertAndSend("/topic/time", new Date().toString());
-    //    }
-
-    class DateTask extends TimerTask {
-        @Override
-        public void run() {
-            WebSocketUtil.this.messageTemplate.convertAndSend("/topic/time", new Date().toString());
-        }
+    @Scheduled(fixedRate = 1000)
+    public void callWebSocket() {
+        logger.debug("send to page");
+        WebSocketUtil.this.messageTemplate.convertAndSend("/topic/time", new Date().toString());
     }
 
 }
